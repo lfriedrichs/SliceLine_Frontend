5a177c24322339177b906ed2fef39865c3c0352e
davidka7 <david.perekopskiy@go.shoreline.edu>
2020-04-13 17:12:02 -0700

    setup our frontend app, navigation and login

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import NavBar from './containers/NavBar';
+import PizzaSlap from './containers/PizzaSlap';
+import Login from './containers/Login';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  state = {
+    user: null,
+    orders: []
+  }
+  loginMethod = (user) => {
+    this.setState({
+      user
+    })
+  }
+  render() {
+
+    return (
+      <div>
+        {!this.state.user ? <Login onLogin={this.loginMethod} user={this.state.user}/> :
+          <Router>
+            <div>
+              <NavBar />
+
+
+              <Route exact path="/pizza" container={PizzaSlap} />
+
+
+            </div>
+
+
+          </Router>}
+      </div>
+    )
+  }
+};
 
 export default App;
