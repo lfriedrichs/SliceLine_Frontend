d8980f0633bba4d04c0af88e2cafb12e569c906d
davidka7 <david.perekopskiy@go.shoreline.edu>
2020-04-15 09:55:54 -0700

    images

--- src/components/MainForm.js
+++ src/components/MainForm.js
@@ -3,95 +3,112 @@ import SauceForm from './SauceForm'
 import CheeseForm from './CheeseForm'
 import ToppingForm from './ToppingForm'
 import GourmetToppingForm from './GourmetToppingForm'
+import SizeForm from './SizeForm'
 
 const DEFAULT_STATE = {
   sauce: '',
   cheese: '',
   toppings: [],
-  gourmet_toppings: []
+  gourmet_toppings: [],
+  size: ''
 }
 
 class MainForm extends Component {
   state = {
-    id: this.props.id,
+    
     ...DEFAULT_STATE
   }
 
   handleChange = (event) => {
-    event.persist()
+
     const itemType = event.target.name;
     const item = event.target.value;
     let value = this.state[`${itemType}`];
 
-    (Array.isArray(value) ? (
+
+    if (value === item) {
+      value = ''
+      
+    }
+
+    else if (value.includes(item)) { value = value.filter(element => element !== item) }
+
+    else {
+      (Array.isArray(value) ? (
         value.push(item)
-        ) : (
-        value = item
+      ) : (
+          value = item
         )
-    )
+      )
+    }
 
     this.setState({
-        [`${itemType}`]: value
+      [`${itemType}`]: value
     })
   }
 
   handleSubmit = (event) => {
-  
+
     event.preventDefault()
     document.getElementById("order-form").reset()
-    this.props.addToOrder(this.state)
+    
+    this.props.addToOrder(this.state, this.props.id)
 
     this.setState({
       ...DEFAULT_STATE
     })
+    
   }
 
   fillForm = (element, type) => {
-    let status;
-    if (Array.isArray(this.state[type])) {
-      status = this.state[type].includes(element)
-    } else {
-      status = this.state[type] === element
-    }
+ 
+   
+    
     return (<div className="field">
-        <div className="ui checkbox">
-            <input
-              id={element}
-              type="checkbox"
-              value={element}
-              name={type}
-              checked={status}
-              onChange={(e) => this.handleChange(e)}
-            />
-            <img src={ require(`../images/cheese/cheese.png`) }height="100px" width="100px" alt={element} />
-            <label htmlFor={element}>{element}</label>
-        </div>
+      <div className="ui checkbox">
+        <input
+          id={element}
+          type="checkbox"
+          value={element}
+          name={type}
+          checked={Array.isArray(this.state[type]) ? this.state[type].includes(element) : 
+            this.state[type] === element}
+          onChange={(e) => this.handleChange(e)}
+        />
+        <img src={require(`../images/${type}/${element}.png`)} height="100px" width="100px" alt={element} />
+        <label htmlFor={element}>{element}</label>
+      </div>
     </div>)
-    }
+  }
 
   render() {
-    return(
+    return (
       <div className="ui raised container segment">
         <h1 className="ui block header">Create a Pizza</h1>
-        <form className="ui form" id="order-form" onSubmit={ this.handleSubmit }>
+        <form className="ui form" id="order-form" onSubmit={this.handleSubmit}>
+          <SizeForm
+            size={this.state.size}
+            fillForm={this.fillForm}
+          />
+
           <SauceForm
-            sauce={ this.state.sauce }
-            fillForm = { this.fillForm }
+            sauce={this.state.sauce}
+            fillForm={this.fillForm}
           />
 
           <CheeseForm
-            cheese={ this.state.cheese }
-            fillForm = { this.fillForm }
+            cheese={this.state.cheese}
+            fillForm={this.fillForm}
           />
 
           <ToppingForm
-            toppings={ this.state.toppings }
-            fillForm = { this.fillForm }
+            toppings={this.state.toppings}
+            fillForm={this.fillForm}
           />
 
           <GourmetToppingForm
-            gourmet_toppings={ this.state.gourmet_toppings }
-            fillForm = { this.fillForm }
+            gourmet_toppings={this.state.gourmet_toppings}
+            fillForm={this.fillForm}
           />
 
           <br />
