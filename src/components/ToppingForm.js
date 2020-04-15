8dbfff846c0ab16b03f89baaa692a921acda8df0
github username <github email address>
2020-04-15 09:41:30 -0700

    updated ingredients

--- src/components/ToppingForm.js
+++ src/components/ToppingForm.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react'
 
-const toppingsArray = ['artichoke hearts', 'avocado', 'broccoli', 'capers', 'green peppers', 'mushrooms', 'onions', 'olives', 'roasted garlic', 'spinach', 'sun dried tomatoes', 'zucchini', 'bacon', 'bbq chicken', 'ham', 'meatballs', 'pepperoni', 'proscuitto', 'salami', 'sausage']
+const toppingsArray = ['kidney beans', 'asparagus', 'celery', 'garlic', 'red pepper', 'capers', 'cherkin', 'broccoli', 'lime', 'green peppers', 'mushrooms', 'onions', 'olives', 'spinach', 'pineapples', 'bacon', 'ham', 'pepperoni', 'salami', 'sausage', 'lamb', 'chicken', 'beef', 'pork']
 
 const ToppingForm = (props) => {
   return (
