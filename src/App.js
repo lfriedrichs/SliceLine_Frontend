ae705b31b05dd769b1b6e355a0259b83156d6038
github username <github email address>
2020-04-16 12:02:24 -0700

    order confirmation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import NavBar from './containers/NavBar';
 import NewOrder from './containers/NewOrder';
+import Orders from './containers/Orders';
+import OrderConfirmation from './components/OrderConfirmation';
 import Login from './containers/Login';
 import {
   BrowserRouter as Router,
@@ -9,23 +11,35 @@ import {
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import background from './images/pixur.png'
+import { Redirect } from 'react-router-dom'
+
 export default class App extends Component {
 
   state = {
     user: null,
-    orders: []
+    orders: [],
+    result: null
   }
   sectionStyle = {
     backgroundImage: `url(${background})`,
     minHeight: '60px',
-    
+
   }
   sectionStyl = {
     backgroundImage: `url(${background})`,
     minHeight: '1500px',
     overflow: 'scroll'
   }
-  
+
+  confirmOrder = (result) => {
+    this.setState({ result: result })
+    window.history.pushState({}, `/${this.state.user.id}/orderconfirmation`, `/${this.state.user.id}/orderconfirmation`)
+  }
+
+  clearResult = () => {
+    this.setState({ result: null })
+  }
+
   login = (user) => {
     fetch(`http://localhost:3000/api/v1/users/${user}`)
       .then(resp => {
@@ -40,30 +54,36 @@ export default class App extends Component {
   }
 
   render() {
-    
+
 
     return (
       <div style={this.sectionStyl}>
         {!this.state.user ? (
-          <Login onLogin={this.login}/> 
-        ) : (
+          <Login onLogin={this.login} />
+        ) : (!this.state.result ? (
           <Router>
             <div>
-            <div style={this.sectionStyle}>
-              <NavBar />
+              <div style={this.sectionStyle}>
+                <NavBar clearResult={this.clearResult} user_name={this.state.user.name}/>
               </div>
-              
-              <Route exact path="/makePizza" render={ (props) => 
-                <NewOrder user_id={this.state.user.id}/>
-              }/>
-             
-              {/* component={NewOrder />  */}
+
+              <Route exact path={`/${this.state.user.name}/neworder`} render={(props) =>
+                <NewOrder user_id={this.state.user.id} confirmOrder={this.confirmOrder} />
+              } />
+
+              <Route exact path={`/${this.state.user.name}/orders`} render={(props) =>
+                <Orders user_name={this.state.user.name} orders={this.state.orders}/>
+              } />
 
             </div>
-          </Router>
-          
-        )}
-       <div className="backy"> </div>
+            {/* welcome user  */}
+          </Router>) : (
+            <Router><div style={this.sectionStyle}>
+              <NavBar clearResult={this.clearResult} user={this.state.user.id}/>
+              <OrderConfirmation user_id={this.state.user.id} result={this.state.result} />
+            </div></Router>
+          ))}
+        <div className="backy"> </div>
       </div>
     )
   }
